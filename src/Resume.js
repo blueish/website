a252e08053d4784d30dfdd943ab87eae8e7bc7ac
Sam C <samchowa@gmail.com>
2017-09-23 23:13:06 -0700

    add gcloud config and another line to resume

--- src/Resume.js
+++ src/Resume.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 const Item = ({ place, time, details }) => (
-    <div className={"resume-item__wrapper"}>
+    <li className={"resume-item__wrapper"}>
         <div className={"resume-workplace"}>{place}</div>
         <div className={"resume-date"}>{time}</div>
         <div className={"resume-details"}>{details}</div>
-    </div>
+    </li>
 );
 
 class Resume extends Component {
@@ -17,6 +17,13 @@ class Resume extends Component {
                     place={"University of British Columbia"}
                     time={"Class of 2019 - Computer Science"}
                     details={"Currently a 3rd year undergraduate studying Computer Science."}
+                />
+                <Item
+                    place={"Cisco Umbrella (formerly OpenDNS)"}
+                    time={"September 2016 - September 2017"}
+                    details={"Developed in a React/Redux frontend alongside a Java backend in an agile environment, pushed production code \n" +
+                    "live to client-facing products. Participated in design meetings and contributed to eliminating bugs, writing tests, \n" +
+                    "and working with several other teams to coordinate product features."}
                 />
             </div>
         );
