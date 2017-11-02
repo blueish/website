f604d630ff380deebbfbea287d9d01ac19fa7bc8
Sam C <samchowa@gmail.com>
2017-11-01 18:17:39 -0700

    some style tweaks & remove unused files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// todo: decide if I need the cached behavior for this app
-// registerServiceWorker();
